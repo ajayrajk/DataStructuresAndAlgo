46d8087bd7746e111ff1212c98308ed57aab29e8
Ajay RajPut <ajayhbti11@gmail.com>
2019-09-15 23:54:29 +0530

    Update BubbleSortingAlgo.java

--- app/src/main/java/com/ajayraj/datastructures/sorting/BubbleSortingAlgo.java
+++ app/src/main/java/com/ajayraj/datastructures/sorting/BubbleSortingAlgo.java
@@ -3,6 +3,19 @@ package com.ajayraj.datastructures.sorting;
 /**
  * apttrix.com
  * Created by Ajay Raj on 01,June,2019
+ 
+ 
+ Algorithm	Time Complexity
+  Best	Average	Worst	
+Selection Sort	Ω(n^2)	θ(n^2)	O(n^2)
+Bubble Sort	Ω(n)	θ(n^2)	O(n^2)
+Insertion Sort	Ω(n)	θ(n^2)	O(n^2)
+Heap Sort	Ω(n log(n))	θ(n log(n))	O(n log(n))
+Quick Sort	Ω(n log(n))	θ(n log(n))	O(n^2)
+Merge Sort	Ω(n log(n))	θ(n log(n))	O(n log(n))
+Bucket Sort	Ω(n+k)	θ(n+k)	O(n^2)
+Radix Sort	Ω(nk)	θ(nk)	O(nk)
+ 
  */
 
 public class BubbleSortingAlgo {
