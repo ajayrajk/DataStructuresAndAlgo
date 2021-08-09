4dc7837aa020354fce76c72f9022abd0a4238236
Ajay RajPut <ajayhbti11@gmail.com>
2021-08-09 16:46:31 +0530

    Update InsertionSortingAlgo.java

--- app/src/main/java/com/ajayraj/datastructures/sorting/InsertionSortingAlgo.java
+++ app/src/main/java/com/ajayraj/datastructures/sorting/InsertionSortingAlgo.java
@@ -5,6 +5,46 @@ package com.ajayraj.datastructures.sorting;
  * Created by Ajay Raj on 07,June,2019
  */
 
+
+
+
+
+
+ 1. First Solution:- 
+public class HelloWorld{
+
+     public static void main(String []args){
+         int arr[]={3, 4, 7, 1, 10, 8, 2, 22, 99, 50};
+          System.out.println("Before sorting ");
+         print(arr);
+         sort(arr);
+           System.out.println("after sorting");
+         print(arr);
+         
+     }
+     
+    static void sort(int arr[]){
+          
+          for(int i=1;i<arr.length ;i++){
+              int key=arr[i];
+              int j=i-1;
+              
+              while(j>=0 && arr[j]>key){
+                  arr[j+1]=arr[j];
+                  j=j-1;
+              }
+              arr[j+1]=key;
+          }
+     }
+     
+   static void print(int arr[]){
+         for (int i=0;i<arr.length ;i++){
+             System.out.print(arr[i]+" ");
+         }
+         System.out.println(" ");
+     }
+}
+ 2: Solution 
 public class InsertionSortingAlgo {
 
     public static void main(String args[]) {
